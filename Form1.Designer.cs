19dba6f38796f263bf768424ff11080cfd79faf6
kevinkentish <kentishiyasawmy@gmail.com>
2020-08-10 09:49:37 +0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace tictactoe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.cell9 = new System.Windows.Forms.Button();
+            this.cell8 = new System.Windows.Forms.Button();
+            this.cell7 = new System.Windows.Forms.Button();
+            this.cell6 = new System.Windows.Forms.Button();
+            this.cell5 = new System.Windows.Forms.Button();
+            this.cell4 = new System.Windows.Forms.Button();
+            this.cell3 = new System.Windows.Forms.Button();
+            this.cell2 = new System.Windows.Forms.Button();
+            this.cell1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.player2score = new System.Windows.Forms.Label();
+            this.player1score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3337F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.3337F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33259F));
+            this.tableLayoutPanel1.Controls.Add(this.cell9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cell8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cell7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cell6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cell5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cell4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cell3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cell1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cell2, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(44, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(327, 238);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // cell9
+            // 
+            this.cell9.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell9.BackColor = System.Drawing.SystemColors.Window;
+            this.cell9.FlatAppearance.BorderSize = 0;
+            this.cell9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell9.Location = new System.Drawing.Point(221, 163);
+            this.cell9.Margin = new System.Windows.Forms.Padding(4);
+            this.cell9.Name = "cell9";
+            this.cell9.Size = new System.Drawing.Size(101, 70);
+            this.cell9.TabIndex = 8;
+            this.cell9.UseVisualStyleBackColor = false;
+            // 
+            // cell8
+            // 
+            this.cell8.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell8.BackColor = System.Drawing.SystemColors.Window;
+            this.cell8.FlatAppearance.BorderSize = 0;
+            this.cell8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell8.Location = new System.Drawing.Point(113, 163);
+            this.cell8.Margin = new System.Windows.Forms.Padding(4);
+            this.cell8.Name = "cell8";
+            this.cell8.Size = new System.Drawing.Size(99, 70);
+            this.cell8.TabIndex = 7;
+            this.cell8.UseVisualStyleBackColor = false;
+            // 
+            // cell7
+            // 
+            this.cell7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell7.BackColor = System.Drawing.SystemColors.Window;
+            this.cell7.FlatAppearance.BorderSize = 0;
+            this.cell7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell7.Location = new System.Drawing.Point(5, 163);
+            this.cell7.Margin = new System.Windows.Forms.Padding(4);
+            this.cell7.Name = "cell7";
+            this.cell7.Size = new System.Drawing.Size(99, 70);
+            this.cell7.TabIndex = 6;
+            this.cell7.UseVisualStyleBackColor = false;
+            // 
+            // cell6
+            // 
+            this.cell6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell6.BackColor = System.Drawing.SystemColors.Window;
+            this.cell6.FlatAppearance.BorderSize = 0;
+            this.cell6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell6.Location = new System.Drawing.Point(221, 84);
+            this.cell6.Margin = new System.Windows.Forms.Padding(4);
+            this.cell6.Name = "cell6";
+            this.cell6.Size = new System.Drawing.Size(101, 70);
+            this.cell6.TabIndex = 5;
+            this.cell6.UseVisualStyleBackColor = false;
+            // 
+            // cell5
+            // 
+            this.cell5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell5.BackColor = System.Drawing.SystemColors.Window;
+            this.cell5.FlatAppearance.BorderSize = 0;
+            this.cell5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell5.Location = new System.Drawing.Point(113, 84);
+            this.cell5.Margin = new System.Windows.Forms.Padding(4);
+            this.cell5.Name = "cell5";
+            this.cell5.Size = new System.Drawing.Size(99, 70);
+            this.cell5.TabIndex = 4;
+            this.cell5.UseVisualStyleBackColor = false;
+            // 
+            // cell4
+            // 
+            this.cell4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell4.BackColor = System.Drawing.SystemColors.Window;
+            this.cell4.FlatAppearance.BorderSize = 0;
+            this.cell4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell4.Location = new System.Drawing.Point(5, 84);
+            this.cell4.Margin = new System.Windows.Forms.Padding(4);
+            this.cell4.Name = "cell4";
+            this.cell4.Size = new System.Drawing.Size(99, 70);
+            this.cell4.TabIndex = 3;
+            this.cell4.UseVisualStyleBackColor = false;
+            // 
+            // cell3
+            // 
+            this.cell3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell3.BackColor = System.Drawing.SystemColors.Window;
+            this.cell3.FlatAppearance.BorderSize = 0;
+            this.cell3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell3.Location = new System.Drawing.Point(221, 5);
+            this.cell3.Margin = new System.Windows.Forms.Padding(4);
+            this.cell3.Name = "cell3";
+            this.cell3.Size = new System.Drawing.Size(101, 70);
+            this.cell3.TabIndex = 2;
+            this.cell3.UseVisualStyleBackColor = false;
+            // 
+            // cell2
+            // 
+            this.cell2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell2.BackColor = System.Drawing.SystemColors.Window;
+            this.cell2.FlatAppearance.BorderSize = 0;
+            this.cell2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell2.Location = new System.Drawing.Point(113, 5);
+            this.cell2.Margin = new System.Windows.Forms.Padding(4);
+            this.cell2.Name = "cell2";
+            this.cell2.Size = new System.Drawing.Size(99, 70);
+            this.cell2.TabIndex = 1;
+            this.cell2.UseVisualStyleBackColor = false;
+            // 
+            // cell1
+            // 
+            this.cell1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cell1.BackColor = System.Drawing.SystemColors.Window;
+            this.cell1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cell1.FlatAppearance.BorderSize = 0;
+            this.cell1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell1.Location = new System.Drawing.Point(5, 5);
+            this.cell1.Margin = new System.Windows.Forms.Padding(4);
+            this.cell1.Name = "cell1";
+            this.cell1.Size = new System.Drawing.Size(99, 70);
+            this.cell1.TabIndex = 0;
+            this.cell1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.player2score);
+            this.panel1.Controls.Add(this.player1score);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.splitter1);
+            this.panel1.Location = new System.Drawing.Point(423, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 232);
+            this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(136, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "PLAYER 2";
+            // 
+            // player2score
+            // 
+            this.player2score.AutoSize = true;
+            this.player2score.Location = new System.Drawing.Point(155, 64);
+            this.player2score.Name = "player2score";
+            this.player2score.Size = new System.Drawing.Size(0, 13);
+            this.player2score.TabIndex = 4;
+            // 
+            // player1score
+            // 
+            this.player1score.AutoSize = true;
+            this.player1score.Location = new System.Drawing.Point(46, 64);
+            this.player1score.Name = "player1score";
+            this.player1score.Size = new System.Drawing.Size(0, 13);
+            this.player1score.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PLAYER 1";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(114, 232);
+            this.splitter1.TabIndex = 0;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(661, 298);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button cell9;
+        private System.Windows.Forms.Button cell8;
+        private System.Windows.Forms.Button cell7;
+        private System.Windows.Forms.Button cell6;
+        private System.Windows.Forms.Button cell5;
+        private System.Windows.Forms.Button cell4;
+        private System.Windows.Forms.Button cell3;
+        private System.Windows.Forms.Button cell2;
+        private System.Windows.Forms.Button cell1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label player2score;
+        private System.Windows.Forms.Label player1score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
